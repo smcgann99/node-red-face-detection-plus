439f8009bb11456e295328e1056fe3d1a2da713f
smcgann99 <52132596+smcgann99@users.noreply.github.com>
2025-02-16 13:14:07 +0000

    edit

--- face-detection.js
+++ face-detection.js
@@ -258,7 +258,7 @@ module.exports = function (RED) {
     function union(box1, box2) {
       const [box1_x1, box1_y1, box1_x2, box1_y2] = box1;
       const [box2_x1, box2_y1, box2_x2, box2_y2] = box2;
-      const box1_area = (box1_x2 - box1_x1) * (box1_y1 - box1_y2);
+      const box1_area = (box1_x2 - box1_x1) * (box1_y2 - box1_y1);
       const box2_area = (box2_x2 - box2_x1) * (box2_y2 - box2_y1);
       return box1_area + box2_area - intersection(box1, box2);
     }
